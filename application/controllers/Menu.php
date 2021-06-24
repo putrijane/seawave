0e6d51420d4dd862e6f134360962a2ab89c9895f
kornelius-veridian <79835686+kornelius-veridian@users.noreply.github.com>
2021-06-24 16:29:11 +0700

    Add files via upload

---
+++ application/controllers/Menu.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Menu extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+
+    public function index()
+    {
+        $data['title'] = 'Menu Pengelolah';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+
+        $this->form_validation->set_rules('menu', 'Menu', 'required');
+
+
+
+
+
+
+
+        if ($this->form_validation->run() == false) {
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('menu/index', $data);
+            $this->load->view('templates/footer',);
+        } else {
+            $this->db->insert('user_menu', ['menu' => $this->input->post('Menu')]);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            berhasil menambahkan data!</div>');
+            redirect('Menu');
+        }
+    }
+}
